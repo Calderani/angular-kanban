f258b8299aa5d5262f8d02b6c4f31153cd77efb8
joao calderani <joao.filho@cbyk.com.br>
2022-10-25 15:50:22 -0300

    feat: first commit

---
+++ src/app/shared/kanban-track/kanban-track.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kanban-track',
+  templateUrl: './kanban-track.component.html',
+  styleUrls: ['./kanban-track.component.scss']
+})
+export class KanbanTrackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
