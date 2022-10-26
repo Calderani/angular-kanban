7d4f431dc7f403db884b3f9d540155a4f6ef2591
joao calderani <joao.filho@cbyk.com.br>
2022-10-26 15:38:45 -0300

    feat: click event and icon

--- src/app/shared/kanban-track/kanban-track.component.ts
+++ src/app/shared/kanban-track/kanban-track.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-kanban-track',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KanbanTrackComponent implements OnInit {
 
+  @Output() public sendOpenTaskModal = new EventEmitter()
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  openTaskModal() {
+    this.sendOpenTaskModal.emit()
   }
 
 }
